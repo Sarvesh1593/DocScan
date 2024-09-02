256c4c756578028b540ed7ba8b43098c5ba9d44a
Sarvesh1593 <sarvesh.in.2003@gmail.com>
2024-09-02 07:25:32 +0530

    push all

--- app/src/main/java/com/mack/docscan/utils/ImageUtils.kt
+++ app/src/main/java/com/mack/docscan/utils/ImageUtils.kt
@@ -1,9 +1,56 @@
 package com.mack.docscan.utils
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 object ImageUtils {
+    private const val TAG = "ImageUtils"
+
+    // Function to load image from URI
+    fun loadImageFromUri(context: Context, uri: Uri?): Bitmap? {
+        if (uri == null) {
+            Log.e(TAG, "URI is null")
+            return null
+        }
+
+        return try {
+            val inputStream = context.contentResolver.openInputStream(uri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            inputStream?.close()
+
+            // Log the bitmap dimensions
+            Log.d(TAG, "Bitmap loaded with dimensions: ${bitmap?.width}x${bitmap?.height}")
+            bitmap
+        } catch (e: Exception) {
+            Log.e(TAG, "Error loading image from URI", e)
+            null
+        }
+    }
+
+    // Function to convert bitmap to URI
+    fun bitmapToUri(context: Context, bitmap: Bitmap): Uri? {
+        // Create an empty file in the cache directory
+        val file = File(context.cacheDir, "image_${System.currentTimeMillis()}.png")
+        return try {
+            // Write the bitmap to the file
+            val stream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.flush()
+            stream.close()
+
+            // Return the Uri for the file
+            Uri.fromFile(file)
+        } catch (e: IOException) {
+            Log.e(TAG, "Error saving bitmap to file", e)
+            null
+        }
+    }
 
     fun decodeSampledBitmapFromFile(
         filePath: String,
--- opencv/build/intermediates/cxx/Debug/3b5n5i1p/logs/x86/configure_command.bat
+++ opencv/build/intermediates/cxx/Debug/3b5n5i1p/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Android\\DocScan\\opencv\\libcxx_helper" ^
+  "-HC:\\Users\\Sarvesh Chaudhary\\StudioProjects\\DocScan\\opencv\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=24" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Android\\sdk\\ndk\\26.1.10909125" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Android\\sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Android\\DocScan\\opencv\\build\\intermediates\\cxx\\Debug\\3b5n5i1p\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Android\\DocScan\\opencv\\build\\intermediates\\cxx\\Debug\\3b5n5i1p\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Sarvesh Chaudhary\\StudioProjects\\DocScan\\opencv\\build\\intermediates\\cxx\\Debug\\3b5n5i1p\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Sarvesh Chaudhary\\StudioProjects\\DocScan\\opencv\\build\\intermediates\\cxx\\Debug\\3b5n5i1p\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Android\\DocScan\\opencv\\.cxx\\Debug\\3b5n5i1p\\x86" ^
+  "-BC:\\Users\\Sarvesh Chaudhary\\StudioProjects\\DocScan\\opencv\\.cxx\\Debug\\3b5n5i1p\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
